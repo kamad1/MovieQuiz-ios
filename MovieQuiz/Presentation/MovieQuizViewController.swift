010462afe1a31134635281904a21230ce2ccbaec
Denis <artkamadi@gmail.com>
2023-05-11 01:08:42 +0300

    корректировка

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -65,11 +65,8 @@ final class MovieQuizViewController: UIViewController {
         counterLabel.text = step.questionNumber
     }
     
-    private func showAnswerResult(isCorrect: Bool) {
-        imageView.layer.masksToBounds = true // 1
-        imageView.layer.borderWidth = 8 // 2
-        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor // 3
-    } 
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -77,72 +74,98 @@ final class MovieQuizViewController: UIViewController {
         
         
     }
-
+    
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true // 1
+        imageView.layer.borderWidth = 8 // 2
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor // 3
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            
+        } else { // 2
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    
+    
 }
+    
+    
+    
+    /*
+     Mock-данные
+     
+     
+     Картинка: The Godfather
+     Настоящий рейтинг: 9,2
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: The Dark Knight
+     Настоящий рейтинг: 9
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: Kill Bill
+     Настоящий рейтинг: 8,1
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: The Avengers
+     Настоящий рейтинг: 8
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: Deadpool
+     Настоящий рейтинг: 8
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: The Green Knight
+     Настоящий рейтинг: 6,6
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: ДА
+     
+     
+     Картинка: Old
+     Настоящий рейтинг: 5,8
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: НЕТ
+     
+     
+     Картинка: The Ice Age Adventures of Buck Wild
+     Настоящий рейтинг: 4,3
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: НЕТ
+     
+     
+     Картинка: Tesla
+     Настоящий рейтинг: 5,1
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: НЕТ
+     
+     
+     Картинка: Vivarium
+     Настоящий рейтинг: 5,8
+     Вопрос: Рейтинг этого фильма больше чем 6?
+     Ответ: НЕТ
+     */
 
-
-
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
