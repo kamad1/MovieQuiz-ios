e9df94dacc42548fb77930a5b024dbd1d4a74e34
KaMaDi <artkamadi@gmail.com>
2023-06-24 14:38:59 +0300

    ТЕСТОВАЯ ВЕТКА СПРИНТ 7 НЕ ПОКАЗЫВАЕТ ФИНАЛЬНЫЙ АЛЕРТ после прохождения квита при отключении интернета не показывает алерт

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -11,21 +11,25 @@ final class MovieQuizPresenter {
     
     var currentQuestion: QuizQuestion?
     weak var viewController: MovieQuizViewController?
-    
+    var correctAnswers = 0
+    var questionFactory: QuestionFactoryProtocol?
+    var alertPresenter: AlertPresenterProtocol?
+    private var statisticService: StatisticService?
     let questionsAmount: Int = 10
-        private var currentQuestionIndex: Int = 0
-        
-        func isLastQuestion() -> Bool {
-            currentQuestionIndex == questionsAmount - 1
-        }
-        
-        func resetQuestionIndex() {
-            currentQuestionIndex = 0
-        }
-        
-        func switchToNextQuestion() {
-            currentQuestionIndex += 1
-        }
+    private var currentQuestionIndex: Int = 0
+    
+    
+    func isLastQuestion() -> Bool {
+        currentQuestionIndex == questionsAmount - 1
+    }
+    
+    func resetQuestionIndex() {
+        currentQuestionIndex = 0
+    }
+    
+    func switchToNextQuestion() {
+        currentQuestionIndex += 1
+    }
     
     func didReceiveNextQuestion(question: QuizQuestion?) {
         guard let question = question else {
@@ -38,7 +42,40 @@ final class MovieQuizPresenter {
         }
     }
     
-     func convert(model: QuizQuestion) -> QuizStepViewModel {
+    func showNextQuestionOrResults() {
+//        imageView.layer.borderWidth = 0
+        if self.isLastQuestion() {
+//            imageView.layer.borderWidth = 8
+            statisticService?.store(correct: correctAnswers, total: self.questionsAmount)
+            guard let gamesCount = statisticService?.gamesCount else { return }
+            guard let bestGame = statisticService?.bestGame else { return }
+            guard let totalAccuracy = statisticService?.totalAccuracy else { return }
+            // QuizResultViewModel
+            
+            let finalScreen = AlertModel (title: "Этот раунд окончен!",
+                                          message: """
+     Ваш результат: \(correctAnswers)/\(self.questionsAmount)
+     Количество сыгранных квизов: \(gamesCount)
+     Рекорд: \(bestGame.correct)/\(bestGame.total) (\(bestGame.date.dateTimeString))
+     Средняя точность: \(String(format: "%.2f", totalAccuracy))%
+     """ ,
+                                          buttonText: "Сыграть еще раз",
+                                          completion: { [weak self] in
+                guard let self = self else { return }
+//                self.imageView.layer.borderWidth = 0
+                self.resetQuestionIndex()
+                self.correctAnswers = 0
+                questionFactory?.requestNextQuestion()
+            })
+            alertPresenter?.showQuizResult(model: finalScreen)
+        } else {
+            self.switchToNextQuestion()
+            questionFactory?.requestNextQuestion()
+        }
+        
+    }
+    
+    func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(data: model.image) ?? UIImage(),
             question: model.text,
@@ -56,13 +93,13 @@ final class MovieQuizPresenter {
     }
     
     private func didAnswer(isYes: Bool) {
-            guard let currentQuestion = currentQuestion else {
-                return
-            }
-            
-            let givenAnswer = isYes
-            
-            viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        guard let currentQuestion = currentQuestion else {
+            return
         }
+        
+        let givenAnswer = isYes
+        
+        viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
 }
 
